f4bcc10df54459a9e04adc30e260b7e913376c3c
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-07-25 23:26:19 -0700

    Done for the day

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -11,3 +11,14 @@ function moreProjects(){
 
     clickCount += 1;    
 }
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    document.getElementById("Top").style.backgroundColor = "rgba(0,0,0,0.25)";
+  } else {
+    document.getElementById("Top").style.backgroundColor = "transparent";
+  }
+}
