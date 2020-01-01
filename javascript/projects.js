8e21e4096a08b6aa6afec4cbb9563f3ce1370c6d
ashvinnagarajan <50090261+ashvinnagarajan@users.noreply.github.com>
2019-12-31 18:55:44 -0800

    Removed scroll

--- javascript/projects.js
+++ javascript/projects.js
@@ -113,9 +113,3 @@ function mainProjects() {
 
 mainProjects();
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  document.getElementById("Top").style.backgroundColor = "initial";
-}
